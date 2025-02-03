7f6fb0209cc533393671206758cbe3e207b47826
znataniel <znataniel@protonmail.com>
2025-02-03 13:51:13 -0300

    add health endpoint and move static to ignored dir

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
+func getHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 
 	const PORT = "8080"
-	const ROOT = "."
+	const ROOT = "static/"
 
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(ROOT)))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(ROOT))))
+	serveMux.HandleFunc("/healthz", getHealthz)
 
 	server := http.Server{
 		Addr:    ":" + PORT,
