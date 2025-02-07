aa2cfd08b174023f9466a6fc24a1cb1a6e55c787
znataniel <znataniel@protonmail.com>
2025-02-07 15:42:37 -0300

    add hits counter for fs endpoint with middleware and reset

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsFs(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getFsHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetFsHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.And(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits reset was successful"))
+}
 
 func getHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -16,9 +42,13 @@ func main() {
 	const PORT = "8080"
 	const ROOT = "static/"
 
+	cfg := apiConfig{}
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(ROOT))))
+	serveMux.Handle("/app/", cfg.middlewareMetricsFs(http.StripPrefix("/app", http.FileServer(http.Dir(ROOT)))))
 	serveMux.HandleFunc("/healthz", getHealthz)
+	serveMux.HandleFunc("/metrics", cfg.getFsHits)
+	serveMux.HandleFunc("/reset", cfg.resetFsHits)
 
 	server := http.Server{
 		Addr:    ":" + PORT,
