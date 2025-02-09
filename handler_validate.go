40a09da065a58c8c431308a8e80e23080ab05e42
znataniel <znataniel@protonmail.com>
2025-02-09 13:46:11 -0300

    bad words cleanup

--- handler_validate.go
+++ handler_validate.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +13,7 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type valid struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -23,11 +25,23 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 
 	if len(ch.Body) > 0 && len(ch.Body) <= 140 {
 		res := valid{
-			Valid: true,
+			CleanedBody: cleanChrip(ch.Body),
 		}
 		respondJson(w, http.StatusOK, res)
 		return
 	}
 
 	respondJsonError(w, http.StatusBadRequest, nil, "chirp is null or too long")
+}
+
+func cleanChrip(body string) string {
+	bannedWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(body, " ")
+	for i, w := range words {
+		if slices.Contains(bannedWords, strings.ToLower(w)) {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+
 }
