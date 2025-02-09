bbe2daef7ed91c463d4cdc83d0ac2ae68cbaa3e5
znataniel <znataniel@protonmail.com>
2025-02-09 02:01:48 -0300

    add validate endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -44,6 +45,58 @@ func getHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	type notValid struct {
+		Error string `json:"error"`
+	}
+
+	type valid struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	ch := chirp{}
+	if err := decoder.Decode(&ch); err != nil {
+		log.Printf("error decoding request body: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	var data []byte
+	var err error
+	w.Header().Set("Content-Type", "application/json")
+
+	if len(ch.Body) > 0 && len(ch.Body) <= 140 {
+		res := valid{
+			Valid: true,
+		}
+		data, err = json.Marshal(res)
+		if err != nil {
+			log.Printf("could not encode data: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(200)
+	} else {
+		res := notValid{
+			Error: "Chirp is too long",
+		}
+		data, err = json.Marshal(res)
+		if err != nil {
+			log.Printf("could not encode data: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(400)
+	}
+
+	w.Write(data)
+}
+
 func main() {
 
 	const PORT = "8080"
@@ -54,6 +107,7 @@ func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/", cfg.middlewareMetricsFs(http.StripPrefix("/app", http.FileServer(http.Dir(ROOT)))))
 	serveMux.HandleFunc("GET /api/healthz", getHealthz)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirp)
 	serveMux.HandleFunc("GET /admin/metrics", cfg.getFsHits)
 	serveMux.HandleFunc("POST /admin/reset", cfg.resetFsHits)
 
