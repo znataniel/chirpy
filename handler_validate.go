b25b48bd21a0634026e25420446f9f78b3d48346
znataniel <znataniel@protonmail.com>
2025-02-09 02:26:34 -0300

    refactor validate handler

--- handler_validate.go
+++ handler_validate.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
 	type chirp struct {
 		Body string `json:"body"`
-	}
-
-	type notValid struct {
-		Error string `json:"error"`
 	}
 
 	type valid struct {
@@ -22,38 +17,17 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	ch := chirp{}
 	if err := decoder.Decode(&ch); err != nil {
-		log.Printf("error decoding request body: %s", err)
-		w.WriteHeader(500)
+		respondJsonError(w, http.StatusInternalServerError, err, "couldn't decode json parameters")
 		return
 	}
-
-	var data []byte
-	var err error
-	w.Header().Set("Content-Type", "application/json")
 
 	if len(ch.Body) > 0 && len(ch.Body) <= 140 {
 		res := valid{
 			Valid: true,
 		}
-		data, err = json.Marshal(res)
-		if err != nil {
-			log.Printf("could not encode data: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.WriteHeader(200)
-	} else {
-		res := notValid{
-			Error: "Chirp is too long",
-		}
-		data, err = json.Marshal(res)
-		if err != nil {
-			log.Printf("could not encode data: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.WriteHeader(400)
+		respondJson(w, http.StatusOK, res)
+		return
 	}
 
-	w.Write(data)
+	respondJsonError(w, http.StatusBadRequest, nil, "chirp is null or too long")
 }
